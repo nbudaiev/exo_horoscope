30bf7b5bba7b0812dca69de89c35a62df068b03a
Nazar Budaiev <bazarsen123@gmail.com>
2024-07-15 16:20:35 -0500

    add checks for lat and log inputs

--- main.py
+++ main.py
@@ -10,12 +10,22 @@ class User(object):
     User class
     """
 
-    def __init__(self, ra, dec, time):
+    def __init__(self, latitude, longitude, time):
         """
         
         """
-        self.ra = ra
-        self.dec = dec
+        if latitude.unit != 'u.deg':
+            raise ValueError('Latitude must be in degrees')
+        if longitude.unit != 'u.deg':
+            raise ValueError('Longitude must be in degrees')
+
+        if latitude.value < -90 or latitude.value > 90:
+            raise ValueError('Latitude must be between -90 and 90')
+        if longitude.value < -180 or longitude.value > 180:
+            raise ValueError('Longitude must be between -180 and 180')
+        
+        self.latitude = latitude
+        self.longitude = longitude
         self.time = Time(time)
 
     def get_zenith(self):
@@ -23,7 +33,7 @@ class User(object):
         Get zenith coordinates
         """
 
-        location = EarthLocation(lat=latitude*u.deg, lon=longitude*u.deg)
+        location = EarthLocation(lat=slef.latitude*u.deg, lon=self.longitude*u.deg)
         zenith = SkyCoord(alt=90*u.deg, az=0*u.deg, frame=AltAz(obstime=time, location=location))
         zenith_radec = zenith.transform_to('icrs')
 
